d75a073cb7000481bb415beeb4498afc97537906
Luke Miller <lukem1293@gmail.com>
2021-04-08 00:09:08 -0500

    working on event card

--- frontend/src/components/EventCard.js
+++ frontend/src/components/EventCard.js
@@ -1,5 +1,4 @@
 import styled from '@emotion/styled'
-import React from 'react'
 import PortableText from './PortableText'
 
 const EventStyles = styled.article`
@@ -11,15 +10,12 @@ const EventStyles = styled.article`
   }
 `
 export default function EventCard({id, title, _rawDescription, location: {lat, lng}, eventType, coffeeTruckUsed, eventStart, eventEnd}) {
+
   return (
     <EventStyles>
-      <iframe
-              src={`https://www.google.com/maps/embed/v1/place?q=${lat},${lng}&zoom=15&key=${process.env.GATSBY_GOOGLE_MAP_API_KEY}&attribution_source=Google+Maps+Embed+API&attribution_web_url=https://developers.google.com/maps/documentation/embed/`}
-              frameBorder="0"
-              width="100%"
-              height="350"
-              title={`Google map `}
-            />
+      {/**
+       * @TODO - change backend schema from geopoint to lcoation/address group
+       */}
       <div>
         <h3>{title}</h3>
         <small>{eventType === 'private' && <span>PRIVATE</span>} {coffeeTruckUsed === 'yes' && <span>coffee truck icon</span>}</small>
